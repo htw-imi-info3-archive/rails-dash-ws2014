8558f5a535454bf105a52fdb5722857c950c8507
B Kleinen <kleinen@infrastructure.de>
2015-01-09 13:23:18 +0100

    rails g scaffold Artist name:string & rake db:migrate

--- SongBook/db/schema.rb
+++ SongBook/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109114713) do
+ActiveRecord::Schema.define(version: 20150109122042) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
