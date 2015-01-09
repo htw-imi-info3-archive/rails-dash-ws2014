9fe5083696a29da3bf2ce0baf0d948f981d2c03b
B Kleinen <kleinen@infrastructure.de>
2015-01-09 13:24:59 +0100

    rails g migration AddArtistIdToSong artist_id:integer

--- SongBook/db/schema.rb
+++ SongBook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150109122042) do
+ActiveRecord::Schema.define(version: 20150109122343) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150109122042) do
     t.string   "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
   end
 
 end
