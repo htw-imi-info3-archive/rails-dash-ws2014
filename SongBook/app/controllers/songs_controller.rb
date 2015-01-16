80302ea7a8f913daf4c80046c5fcc25ace621c42
B Kleinen <kleinen@infrastructure.de>
2015-01-16 09:49:03 +0100

    handle artist names in editing

--- SongBook/app/controllers/songs_controller.rb
+++ SongBook/app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   before_action :set_song, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_song_artist_name, :set_artist_names, only: [:new,:edit]
   # GET /songs
   # GET /songs.json
   def index
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
   # POST /songs.json
   def create
     @song = Song.new(song_params)
-
+    @song.artist = get_artist(params)
     respond_to do |format|
       if @song.save
         format.html { redirect_to @song, notice: 'Song was successfully created.' }
@@ -42,6 +42,7 @@ class SongsController < ApplicationController
   # PATCH/PUT /songs/1
   # PATCH/PUT /songs/1.json
   def update
+    @song.artist = get_artist(params)
     respond_to do |format|
       if @song.update(song_params)
         format.html { redirect_to @song, notice: 'Song was successfully updated.' }
@@ -64,11 +65,26 @@ class SongsController < ApplicationController
   end
 
   private
+    def get_artist(params)
+     artist_name = params["artist_name"]
+     artist = Artist.find_or_create_by(name: artist_name)
+    end
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_song
       @song = Song.find(params[:id])
     end
+    def set_song_artist_name
+      if params["artist_name"]
+        @song_artist_name = get_artist(params).name 
+      else 
+        @song_artist_name = @song.artist ? @song.artist.name : ""
+      end       
+    end
 
+    def set_artist_names
+      @artist_names = Artist.all.map {|a| a.name}
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
       params.require(:song).permit(:title, :duration, :genre)
